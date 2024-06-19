0af5a034402b674cf4f6e7b78cb976e14e1f8950
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2024-06-19 19:54:33 +0200

    react

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,26 +1,13 @@
-import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchContacts, deleteContact } from '../../redux/contactsOps';
+import { deleteContact } from '../../redux/contactsOps';
 import styles from './ContactList.module.css';
 
 const ContactList = () => {
   const contacts = useSelector(state => state.contacts.items);
   const dispatch = useDispatch();
- //const filter = useSelector((state) => state.filters.name);
-  const loading = useSelector((state) => state.contacts.loading);
-  const error = useSelector((state) => state.contacts.error);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
-  //const filteredContacts = contacts.filter(contact =>
-  //  contact.name.toLowerCase().includes(filter.toLowerCase()));
 
   return (
     <>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error}</p>}
       <ul className={styles.list}>
         {contacts.map(({ id, name, number }) => (
           <li key={id} className={styles.item}>
